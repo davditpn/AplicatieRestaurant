5e65ab596ba44dc574b93459d6381f7e67492b60
david <david.tepeneu@tum.de>
2026-01-09 15:13:44 +0200

    Manager

--- Domain/Entities/Manager.cs
+++ Domain/Entities/Manager.cs
@@ -1,6 +1,17 @@
 namespace AplicatieRestaurant.Domain.Entities;
+using System.Text.Json.Serialization;
+using AplicatieRestaurant.Domain.Enums;
 
-public class Manager
+public class Manager : User
 {
-    
+    public Manager(string username, string password) : base(username, password, UserRole.Manager)
+    {
+        
+    }
+
+    [JsonConstructor]
+    public Manager(Guid id, string username, string password, UserRole role) : base(id, username, password, role)
+    {
+        
+    }
 }
