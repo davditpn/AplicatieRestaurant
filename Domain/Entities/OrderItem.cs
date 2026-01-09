6c09c24691ae91b20d47ecd9298e101705eda434
david <david.tepeneu@tum.de>
2026-01-09 15:31:37 +0200

    orderitem

---
+++ Domain/Entities/OrderItem.cs
@@ -0,0 +1,26 @@
+namespace AplicatieRestaurant.Domain.Entities;
+
+public class OrderItem
+{
+    public Guid DishId { get; set; }
+
+    public string DishName { get; set; } = string.Empty;
+    
+    public decimal Price { get; set; }
+    
+    public int Quantity { get; set; }
+
+
+    public OrderItem() //constructor gol pt json
+    {
+        
+    }
+
+    public OrderItem(Guid dishId, string dishName, decimal price, int quantity)
+    {
+        DishId = dishId;
+        DishName = dishName;
+        Price = price;
+        Quantity = quantity;
+    }
+}
