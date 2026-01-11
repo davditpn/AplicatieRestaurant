7528a4a93cfbed5601892effa15b8375c5273819
Tunariu Valentin <tunariuv@yahoo.com>
2026-01-12 00:52:23 +0200

    application part 1

---
+++ Application/ApplicationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AplicatieRestaurant.Application;
+
+public static class ApplicationExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddTransient<RestaurantService>();
+        return services;
+    }
+}
