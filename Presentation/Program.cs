6496a6130dc679dc553437e3e3f54afe5e547733
Tunariu Valentin <tunariuv@yahoo.com>
2026-01-13 00:40:37 +0200

    runui

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -20,4 +20,21 @@ using (var scope = host.Services.CreateScope())
 {
     var service = scope.ServiceProvider.GetRequiredService<RestaurantService>();
     var userRepo = scope.ServiceProvider.GetRequiredService<IRepository<User>>();
-}    
+}   
+
+static void RunUI (RestaurantService service, IRepository<User> userRepo)
+{
+    while (true)
+    {
+        Console.WriteLine("\n=== APLICAȚIE RESTAURANT ===");
+        Console.WriteLine("1. Client (Plasează comandă)");
+        Console.WriteLine("2. Manager (Gestionează meniul și comenzile)");
+        Console.WriteLine("0. Ieșire");
+        Console.Write("Alege: ");
+        var key= Console.ReadLine();
+
+        if (key == "0") break;
+        if (key == "1") RunClient(service, userRepo);
+        if (key == "2") RunManager(service);
+    }
+}
