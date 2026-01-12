afe91cc804b40e016df1f2cced28e04dc0acc659
david <david.tepeneu@tum.de>
2026-01-12 21:27:06 +0200

    generic host

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using AplicatieRestaurant.Application;
+using AplicatieRestaurant.Domain.Entities;
+using AplicatieRestaurant.Domain.Enums;
+using AplicatieRestaurant.Domain.Interfaces;
+using AplicatieRestaurant.Infrastructure;
 
-Console.WriteLine("Hello, World!");
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddInfrastructure();
+        services.AddApplication();
+        services.AddLogging(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Information));
+    })
+    .Build();
+
+using (var scope = host.Services.CreateScope())
+{
+    var service = scope.ServiceProvider.GetRequiredService<RestaurantService>();
+    var userRepo = scope.ServiceProvider.GetRequiredService<IRepository<User>>();
+}    
